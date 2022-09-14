613771efe67de912f8069e5dd3a550d47af7a961
Fran Manez <francisco.manez@upc.edu>
2022-09-14 05:57:16 +0200

    estructuras de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product} from "./product.model";
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,41 @@ export class AppComponent {
     age: 10,
     img: 'https://www.w3schools.com/howto/img_avatar.png'
   }
+  names: string[] = ['Fran', 'Juli', 'Ana'];
+  newName = '';
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/bike.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/bike.jpg'
+    }
+  ]
 
   toggleButton(){
     this.btnDisabled = !this.btnDisabled
@@ -20,5 +56,20 @@ export class AppComponent {
 
   incAge(){
     this.person.age++
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
+  addName(){
+    this.names.push(this.newName)
+    this.newName = ''
+  }
+
+  deleteName(index:number){
+    this.names.splice(index, 1  )
+    this.newName = ''
   }
 }
